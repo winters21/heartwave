5fa93241214481d5800be294cdcfaad46514106e
winters21 <icehawk246@gmail.com>
2023-03-28 14:03:24 -0400

    Created class files

---
+++ final/coherence.h
@@ -0,0 +1,11 @@
+#ifndef COHERENCE_H
+#define COHERENCE_H
+
+
+class Coherence
+{
+public:
+    Coherence();
+};
+
+#endif // COHERENCE_H
---
+++ final/log.h
@@ -0,0 +1,11 @@
+#ifndef LOG_H
+#define LOG_H
+
+
+class Log
+{
+public:
+    Log();
+};
+
+#endif // LOG_H
