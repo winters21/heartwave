592f9dbd0e59957399a4ec0b2089a6decc7001f1
student <jake.diedrich@gmail.com>
2023-03-31 13:42:30 -0400

    Implemented Log Class

--- final/log.cpp
+++ final/log.cpp
@@ -1,6 +1,47 @@
 #include "log.h"
 
-Log::Log()
-{
+Log::Log(){}
 
+/**
+ * @brief Log::addToSystemLog
+ * This method adds the logs to a txt file at the end of a session, it then resets the sessions logs
+ */
+void Log::addToSystemLog() {
+    ofstream file("../Session.txt");  // TODO: add unique id depending if the files get overridden
+
+    // Loop through the vector and add the logs to the output file
+    for (string log: currentSession) {
+        file << log;
+    }
+
+    file.close();
+    resetCurrentSession();
+}
+
+/**
+ * @brief Log::addToCurrentLogs
+ * @param log
+ * Adds a timestamp to the log and adds it to the current sessions logs
+ */
+void Log::addToCurrentLogs(string log) {
+    time_t t = time(0);
+    string currTime = ctime(&t);
+
+    // Remove the \n character from the current time so the log does not appear on a new line
+    if (!currTime.empty()) {
+        if (currTime[currTime.length() - 1] == '\n')
+            currTime.erase(currTime.length() - 1);
+    }
+
+    string fullLog = currTime + " -- " + log;
+    cout << fullLog << endl;
+    currentSession.push_back(fullLog);
+}
+
+/**
+ * @brief Log::resetCurrentSession
+ * Resets the current sessiin logs to make room for a new sessions logs
+ */
+void Log::resetCurrentSession() {
+    currentSession.clear();
 }
--- final/log.h
+++ final/log.h
@@ -1,11 +1,23 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include<vector>
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<fstream>
+
+using namespace std;
 
 class Log
 {
 public:
     Log();
+    void addToCurrentLogs(string);
+    void addToSystemLog();
+    void resetCurrentSession();
+private:
+    vector<string> currentSession;
 };
 
 #endif // LOG_H
