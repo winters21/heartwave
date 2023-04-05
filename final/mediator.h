d22a62040d0b486f38be6f00fa80a0dc23977bf8
student <jake.diedrich@gmail.com>
2023-04-05 11:44:31 -0400

    Setup Basic UI Features

--- final/mediator.h
+++ final/mediator.h
@@ -1,11 +1,18 @@
 #ifndef MEDIATOR_H
 #define MEDIATOR_H
 
+#include<iostream>
+#include "heartwave.h"
+
+using namespace std;
 
 class Mediator
 {
 public:
     Mediator();
+    void updateSettings(int, int);
+private:
+    HeartWave *hv;
 };
 
 #endif // MEDIATOR_H
